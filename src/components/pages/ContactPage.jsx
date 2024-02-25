339af167458891d6f0e41c37048dd09405a50b63
rikfkiahmadfahrezi <rikfkiaf589@gmail.com>
2024-02-25 20:22:51 +0700

    create contact page & add routes

---
+++ src/components/pages/ContactPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ContactSection from '../templates/contactSection'
+import Footer from '../templates/footer'
+import Cursor from '@/UI/elements/Cursor'
+
+function ContactPage() {
+  return (
+   <>
+     <ContactSection />
+     <Footer />
+
+
+     <Cursor />
+   </>
+  )
+}
+
+export default ContactPage
