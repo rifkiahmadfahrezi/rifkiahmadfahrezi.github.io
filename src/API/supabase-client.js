666c079cd21f3f1649ea0084b34667a612648b40
rikfkiahmadfahrezi <rikfkiaf589@gmail.com>
2024-03-14 13:46:58 +0700

    auth

--- src/API/supabase-client.js
+++ src/API/supabase-client.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_KEY
+  'https://brppdjgvxfgszqavezdm.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJycHBkamd2eGZnc3pxYXZlemRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDMzNzQzMTgsImV4cCI6MjAxODk1MDMxOH0.iB0I-lZZGbcfpVQdyspr1IRsqaZvLy9q27hiiMXx4_M'
 );
 
 export default supabase;
